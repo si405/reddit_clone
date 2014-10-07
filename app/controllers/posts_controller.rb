c9a2a33d6547a9e88714c6af8ad7db74cae38f85
Simon Williams <siimon_williams405@yahoo.coms>
2014-10-07 11:13:41 -0700

    Basic functionality for new and edit working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,14 +6,37 @@ class PostsController < ApplicationController
 	end
 
 	# Create a new post using the params provided from 
-	# the view
+	# the view and redirect back to the main index page
 	def create
-		binding.pry
+		# binding.pry
 		@post = Post.create(post_params)
 		redirect_to @posts
 	end
 	
+	# Set @posts to contain all the saved posts
 	def index
+		@posts = Post.all
+	end
+
+	# Params contains the data being passed back from the view
+	def edit
+		binding.pry
+		@post = Post.find(params[:id])
+	end
+
+	# Update the modified post and redirect back to main index
+	# Due to the lack of state we first need to relocate the 
+	# selected post and then perform the update
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		# I tried putting redirect_to @posts but it came up with an error so 
+		# need to look in to why that happened. Using @post/show instead
+		redirect_to @post
+	end
+
+	# Show all saved posts
+	def show
 		@posts = Post.all
 	end
 
