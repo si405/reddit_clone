1fdc438ca3b3f410b48fc0a9dfc571eefa94aa65
Simon Williams <siimon_williams405@yahoo.coms>
2014-10-12 17:52:27 -0700

    Part 4 - Search in comments now working

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,8 @@ class Post < ActiveRecord::Base
 	# to create the search criteria. Search the post title and url
 	def self.search(search)
   		search_condition = '%' + search + '%'
-  		Post.where("post_title LIKE '#{search_condition}' or
-  		 post_url LIKE '#{search_condition}'")
+  		Post.joins(:comments).distinct. where("post_title LIKE '#{search_condition}' or
+  		 	post_url LIKE '#{search_condition}' or
+  		 	body LIKE '#{search_condition}'")
 	end
 end
